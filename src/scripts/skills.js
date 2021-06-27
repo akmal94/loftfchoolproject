70a8422d208a1d70c7e6c352adeecb7b5f890c27
akmal <iskakov.akmal@gmail.com>
2021-06-27 22:43:00 +0600

    блок skills на vue

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,39 @@
-console.log('this is the skills module');
+import Vue from "vue";
+
+const skillsItem = {
+  props: ["skill"],
+  template: "#skills-item",
+  methods: {
+      setCirclePercents(){
+        const circle = this.$refs["colored-circle"];
+        circle.style.strokeDashoffset = this.skill.percent;
+      }
+  },
+  mounted() {
+      this.setCirclePercents();
+  },
+};
+
+const skillsRow = {
+  props: ["category"],
+  template: "#skills-row",
+  components: {
+    skillsItem,
+  },
+};
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  components: {
+    skillsRow,
+  },
+  data() {
+    return {
+      skills: [],
+    };
+  },
+  created() {
+    this.skills = require("../data/skills.json");
+  },
+});
